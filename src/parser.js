4e8f3c6fbc7188709156378854e47dce663e9feb
Zyrael <etoyaleha@gmail.com>
2022-03-26 19:31:56 +0300

    fixed parser

--- src/parser.js
+++ src/parser.js
@@ -2,7 +2,7 @@ import uniqueId from 'lodash/uniqueId';
 
 export default (contents) => {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(response, 'text/xml');
+  const doc = parser.parseFromString(contents, 'text/xml');
   if (!doc.querySelector('rss')) throw new Error('not an RSS');
   const feedTitle = doc.querySelector('title').textContent;
   const feedDescription = doc.querySelector('description').textContent;
